3a088c1f6b6de57df71b18e22fd18e96156ac10e
estellesusanto <estelle.susanto@gmail.com>
2019-11-13 12:17:20 -0600

    baseline code

---
+++ front-end/express-default/server.js
@@ -0,0 +1,18 @@
+// hosts it on the express web
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// creates a virtual path prefix called 'static
+// to serve static files from the 'public' directory
+// ex. http://localhost:8080/static/img/kitten.jpg
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// routes roots path to serve specific file
+// express
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// starts server on port 8080
+app.listen('8080', () => console.log("listening on port 8080"));
